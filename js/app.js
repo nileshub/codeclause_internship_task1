441116bce8461c2c0900c7ac2afd64a3ccd58b1c
nileshub <108585271+nileshub@users.noreply.github.com>
2023-01-01 00:35:57 +0530

    initial files

---
+++ js/app.js
@@ -0,0 +1,8 @@
+function talk(){
+    if(document.getElementById("mail").value==''){
+        alert("please enter your email!")
+    }
+    else{
+    alert("Our Team will contact you shortly,thank you for showing intrest :)")
+    }
+}
